bb6b19ce23adf48daccf65677a8c141fe7d5297e
Leonardo <leomaltadesa0712@gmail.com>
2023-10-03 19:02:45 -0300

    feat: first commit, adding flask and first template

---
+++ jogoteca.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+
+def ola():
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista_de_jogos = [jogo1, jogo2, jogo3]
+    return render_template('lista.html',titulo = 'Jogos', jogos = lista_de_jogos)
+
+app.run(host='0.0.0.0', port=8080)
